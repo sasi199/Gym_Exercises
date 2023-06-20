d4bfe96187ab72ca19b6fbc62a527dba2a3d2ed2
sasi199 <131953754+sasi199@users.noreply.github.com>
2023-06-20 15:04:26 +0530

    firstd coomit

---
+++ Desktop/Gym_Excercise/gym/src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': '808f85413cmshca17d96a4fae6adp1e92acjsnc5c440af3927',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
